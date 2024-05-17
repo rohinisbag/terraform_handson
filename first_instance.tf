389c805ceed517fa3aac285b00c85db7ba7b35c8
Rohinisbag <rohinisbag2898@gmail.com>
2024-05-17 23:21:27 +0530

    first commit

---
+++ first_instance.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "intro" {
+  ami                    = "ami-001843b876406202a"
+  instance_type          = "t2.micro"
+  availability_zone      = "ap-south-1a"
+  key_name               = "dove-key"
+  vpc_security_group_ids = ["sg-02a108efb4a34e9fc"]
+  tags = {
+    Name        = "Dove-Instance",
+    pagal_aurat = "Rohini"
+  }
+
+}
+
+
