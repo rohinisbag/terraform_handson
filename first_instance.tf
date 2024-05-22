406e8f3c962549f60b36815eaccd108d55cc24be
rohinisbag <149901855+rohinisbag@users.noreply.github.com>
2024-05-22 18:16:36 +0530

    Update first_instance.tf

--- first_instance.tf
+++ first_instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "intro" {
   ami                    = "ami-001843b876406202a"
   instance_type          = "t2.micro"
   availability_zone      = "ap-south-1a"
-  key_name               = "dove-key"
+  key_name               = "dove_key"
   vpc_security_group_ids = ["sg-02a108efb4a34e9fc"]
   tags = {
     Name        = "Dove-Instance",
