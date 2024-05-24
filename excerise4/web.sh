77660a50cb06a676526be82f5d4b78b0f0fff1e0
rohinisbag <149901855+rohinisbag@users.noreply.github.com>
2024-05-24 19:00:05 +0530

    Update web.sh

--- excerise4/web.sh
+++ excerise4/web.sh
@@ -3,6 +3,7 @@ sudo yum install wget unzip httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/sperig.zipn
-unzip -o 2117_infinite_loop.zip
-sudo cp -r 2117_infinite_loop/* /var/www/html/
+unzip -o spering spering.zip
+sudo cp -r spering-html/* /var/www/html/
 sudo systemctl restart httpd
+
