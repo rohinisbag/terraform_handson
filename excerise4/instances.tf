8975dbc0852cb740ad80216b87146930d45829ae
rohinisbag <149901855+rohinisbag@users.noreply.github.com>
2024-05-24 19:16:58 +0530

    Update instances.tf

--- excerise4/instances.tf
+++ excerise4/instances.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "dove-inst" {
       "sudo systemctl enable httpd",
       "wget https://www.free-css.com/assets/files/free-css-templates/download/page296/sperig.zip",
       "unzip -o spering spering.zip",
-      "sudo cp -r spering-html/* /var/www/html/",
+      "sudo cp -r spering/spering-html/* /var/www/html/",
       "sudo systemctl restart httpd"
     ]
   }
