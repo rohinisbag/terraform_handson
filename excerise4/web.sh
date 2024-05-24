3366625c7d7a3e27e19f4f66b78984fecc6c2b76
rohinisbag <149901855+rohinisbag@users.noreply.github.com>
2024-05-24 19:38:30 +0530

    Update web.sh

--- excerise4/web.sh
+++ excerise4/web.sh
@@ -2,8 +2,8 @@
 sudo yum install wget unzip httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-wget https://www.free-css.com/assets/files/free-css-templates/download/page296/sperig.zip
-unzip -o spering spering.zip
-sudo cp -r spering-html/* /var/www/html/
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/healet.zip
+unzip -o healet.zip
+sudo cp -r healet-html/* /var/www/html/
 sudo systemctl restart httpd
 
