e5f4af4d7de970563e9dffe4e9f31ee1cbc55459
rohinisbag <149901855+rohinisbag@users.noreply.github.com>
2024-05-24 18:58:11 +0530

    Update web.sh

--- excerise4/web.sh
+++ excerise4/web.sh
@@ -2,7 +2,7 @@
 sudo yum install wget unzip httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/sperig.zipn
 unzip -o 2117_infinite_loop.zip
 sudo cp -r 2117_infinite_loop/* /var/www/html/
 sudo systemctl restart httpd
