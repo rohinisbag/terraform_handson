37507b3f1d8c23358a5278b7b7eae94cbd168fb1
rohinisbag <149901855+rohinisbag@users.noreply.github.com>
2024-05-24 19:05:53 +0530

    Update web.sh

--- excerise4/web.sh
+++ excerise4/web.sh
@@ -2,7 +2,7 @@
 sudo yum install wget unzip httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-wget https://www.free-css.com/assets/files/free-css-templates/download/page296/sperig.zipn
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/sperig.zip
 unzip -o spering spering.zip
 sudo cp -r spering-html/* /var/www/html/
 sudo systemctl restart httpd
